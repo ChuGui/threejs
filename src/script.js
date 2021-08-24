52c555d1bde4454bcb2c113d2fd318c570e8b140
Guillaume Churlet <gchurlet@gmail.com>
2021-08-24 15:21:42 +0200

    Ajout du controls panel

--- src/script.js
+++ src/script.js
@@ -51,6 +51,10 @@ pointLight2.intensity = 1
 scene.add(pointLight2)
 
 gui.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+gui.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
+gui.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
+gui.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
+
 
 /**
  * Sizes
